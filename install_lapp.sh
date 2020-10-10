91c81fe83beed2bcdf745ace1911294534a2def8
Rashid Razak <bib.nexus@gmail.com>
2020-10-10 10:43:14 +0800

    Finish script

--- install_lapp.sh
+++ install_lapp.sh
@@ -12,6 +12,7 @@ NC="\033[0m"
 main() {
     rootCheck
     displayIntro
+    readVar
 
     cd /tmp
 
@@ -19,6 +20,10 @@ main() {
     updateOs
     installEssentialPackages
     installAndConfigureApache
+    installAndConfigurePostgresql
+    installAndConfigurePhp
+    downloadAndInstallMoodle
+    createSudoUser
 }
 #######################################################
 ##  END Main
@@ -38,6 +43,10 @@ rootCheck() {
         echo "Sorry must be in root to run this script"
         exit 65
     fi
+}
+
+readVar() {
+    set -o allexport; source .env; set +o allexport
 }
 
 displayIntro() {
@@ -74,7 +83,7 @@ installEssentialPackages() {
     alternatives --set python "$(which python3)"
     
     # Install essential packages
-    dnf -y install wget unzip git bashtop
+    dnf -y install wget unzip git bashtop rsync
 }
 
 installAndConfigureApache() {
@@ -98,7 +107,7 @@ installAndConfigureApache() {
 
 installAndConfigurePostgresql() {
     # Install PostgreSQL 10
-    dnf -y install @postgres:10
+    dnf -y install @postgresql:10
     dnf -y install postgresql-contrib
 
     systemctl enable postgresql
@@ -108,10 +117,16 @@ installAndConfigurePostgresql() {
     cp /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.bak
 
     # Initialize database
-    runuser -l postgres -c "psql -c \"CREATE USER moodleuser WITH PASSWORD 'yourpassword';\""
-    runuser -l postgres -c "psql -c \"CREATE DATABASE moodle WITH OWNER moodleuser;\""
+    runuser -l postgres -c "psql -c \"CREATE USER $PGSQL_MOODLE_USER WITH PASSWORD '$PGSQL_MOODLE_USER_PASSWORD';\""
+    runuser -l postgres -c "psql -c \"CREATE DATABASE $PGSQL_MOODLE_DATABASE WITH OWNER $PGSQL_MOODLE_USER ENCODING 'UTF8' LC_COLLATE='en_US.UTF-8' LC_CTYPE='en_US.UTF-8' TEMPLATE=template0;\""
 
     echo -e "Installed PostgreSQL: \n$(postgres --version)"
+
+    rm /var/lib/pgsql/data/pg_hba.conf
+    mv $(pwd)/config/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
+    chown postgres:postgres /var/lib/pgsql/data/pg_hba.conf
+    chmod 600 /var/lib/pgsql/data/pg_hba.conf
+    systemctl restart postgresql
 }
 
 installAndConfigurePhp() {
@@ -122,6 +137,53 @@ installAndConfigurePhp() {
     php-pgsql php-pdo_pgsql
 
     echo -e "Installed PHP: \n$(php --version)"
+}
+
+downloadAndInstallMoodle() {
+    git clone -b $MOODLE_STABLE_BRANCH git://git.moodle.org/moodle.git
+    rsync -avP $(pwd)/moodle/ /var/www/html/
+
+    mkdir -p /opt/moodle/moodledata
+    chown -R apache:apache /opt/moodle
+    chmod -R 777 /opt/moodle
+
+    chcon -t httpd_sys_content_t /opt/moodle/moodledata -R
+    chcon -t httpd_sys_rw_content_t /opt/moodle/moodledata -R
+
+    chown -R apache:apache /var/www/html
+    runuser -l apache -c $(which php) -- /var/www/html/admin/cli/install.php \
+        --chmod="$MOODLE_DIRECTORYPERMISSIONS" \
+        --lang="$MOODLE_LANG" \
+        --wwwroot="$MOODLE_WWWROOT" \
+        --dataroot="$MOODLE_DATAROOT" \
+        --dbtype="$MOODLE_DBTYPE" \
+        --dbhost="$MOODLE_DBHOST" \
+        --dbname="$PGSQL_MOODLE_DATABASE" \
+        --dbuser="$PGSQL_MOODLE_USER" \
+        --dbpass="$PGSQL_MOODLE_USER_PASSWORD" \
+        --dbport="$MOODLE_DBPORT" \
+        --prefix="$MOODLE_PREFIX" \
+        --fullname="$MOODLE_FULLNAME" \
+        --shortname="$MOODLE_SHORTNAME" \
+        --summary="$MOODLE_SUMMARY" \
+        --adminuser="$MOODLE_ADMINUSER" \
+        --adminpass="$MOODLE_ADMINPASS" \
+        --adminemail="$MOODLE_ADMINEMAIL"
+        --non-interactive \
+        --agree-license \
+        --allow-unstable
+
+    chown -R root:root /var/www/html
+    chmod -R 755 /var/www/html
+
+    systemctl restart postgresql
+    systemctl restart httpd
+}
+
+createSudoUser() {
+    SUDO_USER_ENCRYPTED_PASSWORD=$(perl -e 'print crypt($SUDO_USER_PASSWORD, "password")' $password)
+    useradd -m -p "$SUDO_USER_USERNAME" "$SUDO_USER_ENCRYPTED_PASSWORD"
+    usermod -aG wheel "$SUDO_USER_USERNAME"
 }
 
 #######################################################
